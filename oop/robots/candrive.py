8ca14f2bdeae16b0fcd2f754cd63c8eb2b715a6f
Aleksander <petr0vskjy.aleksander@gmail.com>
2020-12-26 13:51:06 +0300

    Update candrive.py

--- oop/robots/candrive.py
+++ oop/robots/candrive.py
@@ -6,12 +6,22 @@ class CanDrive():
 
     def __repr__(self):
         return f'{self.__class__.__name__}("{self.number_of_wheels}")'  
-              
+
+    @property
+    def number_of_wheels(self):
+        return self.__number_of_wheels
+
+    @number_of_wheels.setter
+    def number_of_wheels(self, new_number_of_wheels):
+        if not isinstance(new_number_of_wheels, int ):
+                raise ValueError('Количество колесь рбота принадлежит к типу int')
+        self.__number_of_wheels = new_number_of_wheels
+
+
     @property
     def can_drive(self):
         print("могу ехать ")    
 
     
-
 
 
