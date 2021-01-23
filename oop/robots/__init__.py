a595c605c8aad077e988a9ba6886b3fb2f5c73ce
Aleksander <petr0vskjy.aleksander@gmail.com>
2021-01-23 19:03:29 +0300

    Update __init__.py

--- oop/robots/__init__.py
+++ oop/robots/__init__.py
@@ -1,3 +1,4 @@
+# 1
 from  .fatherbot  import FatherBot      # архетип-метакласс
 from  .bot        import Bot            # робот
 from  .candrive   import CanDrive       # класс-миксин обеспечивающий возможность ехать по поверхности
@@ -5,3 +6,5 @@ from  .canfloat   import CanFloat       # класс-миксин обеспеч
 from  .canfly     import CanFly         # класс-миксин обеспечивающий возможность плыть 
 from  .roverbot   import RoverBot       # самодвижущийся робот-автомобиль
 from  .flyingboat import FlyingBoatBot  # летающая лодка-робот
+from  .engine     import Engine, base_engine, aviation_engine, marine_engine, hi_power_engine # датакласс для движков
+
