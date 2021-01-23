ea9dbc5e6f6e03c67c1759bec8c3c9484e2b24dd
Aleksander <petr0vskjy.aleksander@gmail.com>
2021-01-23 19:07:02 +0300

    Create canfly.py

---
+++ oop/robots/canfly.py
@@ -0,0 +1,25 @@
+# для плавающих роботов 
+class CanFloat():
+    """
+    класс-миксин, дающий роботу возможности плыть
+    """
+    def __init__(self, displacement):
+        self.displacement = displacement
+
+    def __repr__(self):
+         return f'{self.__class__.__name__}("{self.displacement}")'  
+
+    @property
+    def displacement(self):
+        return self.__displacement
+
+    @displacement.setter
+    def displacement(self, new_displacement):
+        if not isinstance(new_displacement, float ):
+                raise ValueError('Водоизмещение принадлежит к типу flaot')
+        self.__displacement = new_displacement
+
+
+    @property
+    def can_float(self):
+        print("могу плыть")    
