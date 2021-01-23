1cced9fb2d79b01c6ab74d864d577f8062fee727
Aleksander <petr0vskjy.aleksander@gmail.com>
2021-01-23 19:04:38 +0300

    Update bot.py

--- oop/robots/bot.py
+++ oop/robots/bot.py
@@ -1,26 +1,30 @@
 from pprint import pprint
-from .fatherbot import FatherBot  
+from .fatherbot import FatherBot
+from .engine import Engine, base_engine  
+from .fuel_exception import LowFuelError
 
 class Bot(FatherBot):
-    def __init__(self, model, manufacture, name, weight, engine_power, fuel_supply, fuel_rate):
+    """
+    Базовый класс роботов без выбора способа передвижения
+    """
+    def __init__(self, model, manufacture, name, weight, fuel_supply, engine=base_engine):
         #FatherBot.__init__(self, model, manufacture)
         super().__init__(model, manufacture)
         self.name = name                        # имя робота
         self.weight = weight                    # вес
-        self.engine_power = engine_power        # мощность двигателя
         self.fuel_supply = fuel_supply          # запас топлива
-        self.fuel_rate = fuel_rate              # расход топлива на 100 км
-        self.__spec_engin_power = None          # если еще не вычисляли spec_engin_power
+        self.engine = engine                    # двигатель из датакласса
+        self.__spec_engin_power = None          # удельная мощность движка
 
     def __repr__(self):
-        return f'{self.__class__.__name__}("{self.model, self.manufacture, self.name,  self.weight, self.engine_power, self.fuel_supply, self.fuel_rate})'    
+        return f'{self.__class__.__name__}("{self.model, self.manufacture, self.name,  self.weight,  self.fuel_supply, self.engine.type})'    
 
     @property
     def check_fuel(self):                       # проверка остатка топлива
         if self.fuel_supply <= 0:
             raise ValueError("Топливо закончилось, ищем гравицапу!")
         else:
-            print(f"Можем ехать, мощность топливных элементов = {self.fuel_supply}")
+            print(f"Запас топлива = {self.fuel_supply}")
             return self.fuel_supply
 
     @property 
@@ -44,17 +48,7 @@ class Bot(FatherBot):
         self.__weight = new_weight
         self.__spec_engin_power = None   
 
-    @property
-    def engine_power(self):
-        return self.__engine_power
-
-    @engine_power.setter                            # при изменении мощности вызовем пересчет удельной мощности 
-    def engine_power(self, new_engine_power):
-        if not isinstance(new_engine_power, int):
-                raise ValueError('Имя робота должно быть строковым значением')
-        self.__engine_power = new_engine_power
-        self.__spec_engin_power = None
-
+    
     @property 
     def fuel_supply(self):
         return self.__fuel_supply
@@ -65,23 +59,24 @@ class Bot(FatherBot):
                 raise ValueError('Запас топлива должен быть int')
         self.__fuel_supply = new_fuel_supply
 
-    @property 
-    def fuel_rate(self):
-        return self.__fuel_rate
-
-    @fuel_rate.setter
-    def fuel_rate(self, new_fuel_rate):    
-        if not isinstance(new_fuel_rate, int):
-                raise ValueError('Расход топлива должен быть int')
-        self.__fuel_rate = new_fuel_rate    
-
     @property
     def power_to_weight(self):                          # вычисление удельной мощности движка 
         if self.__spec_engin_power is None:             # если еще не вычисляли spec_engin_power
-            spec_engin_power = self.__weight / self.__engine_power
-            print(f"Удельная мощность двигателя {spec_engin_power}") 
-        return spec_engin_power
+            spec_engin_power = self.__weight / self.engine.engine_power
+            #print(f"Удельная мощность двигателя {spec_engin_power}") 
+            return spec_engin_power    
 
+    
     @property
     def get_capability(self):                           # самореклама - а я еще могу ...
         print(f"Возможности: ", self.__repr__)
+
+    def go_to(self, distance):                      # движение, при наличии топлива
+        try:
+            required_fuel_supply = distance *  self.engine.fuel_rate
+            if required_fuel_supply > self.fuel_supply:
+                delta_fuel = required_fuel_supply - self.fuel_supply
+                raise LowFuelError(self.fuel_supply, delta_fuel)
+            print("Топлива достаточно. Поехали!!!")
+        except LowFuelError as err:
+            print(f"Не могу выполнить команду. Запас топлива: {err.args[0]}, требуется добавить {err.args[1]}")        
