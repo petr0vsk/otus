b0e5f51c7edd45a8a7e6556a3e10c26280521764
Aleksander <petr0vskjy.aleksander@gmail.com>
2021-01-23 19:10:03 +0300

    Update flyingboat.py

--- oop/robots/flyingboat.py
+++ oop/robots/flyingboat.py
@@ -1,21 +1,26 @@
-# робот-амфибия. умеет плавать и летать.
-
 from .fatherbot import FatherBot
 from .bot import Bot
 from .canfloat import CanFloat
 from .canfly import  CanFly
+from .engine import Engine, base_engine, aviation_engine, marine_engine, hi_power_engine 
+
 class FlyingBoatBot(Bot, CanFloat, CanFly):
-    def __init__(self, model, manufacture, name, weight, engine_power, fuel_supply, fuel_rate, displacement, number_of_wings):
+    """
+    Класс робот летающая лодка
+    """
+    def __init__(self, model, manufacture, name, weight, fuel_supply, engine, displacement, number_of_wings):
         FatherBot.__init__(self, model, manufacture)
-        Bot.__init__(self, model, manufacture, name, weight, engine_power, fuel_supply, fuel_rate)
+        Bot.__init__(self, model, manufacture, name, weight, fuel_supply, engine=aviation_engine)
         CanFloat.__init__(self, displacement) 
         CanFly.__init__(self, number_of_wings)
 
 
     def __repr__(self):
-        return f'{self.__class__.__name__}("{self.model, self.manufacture, self.name, self.weight, self.engine_power, self.fuel_supply, self.fuel_rate, self.displacement, self.number_of_wings})' 
+        return f'{self.__class__.__name__}("{self.model, self.manufacture, self.name, self.weight, self.fuel_supply, self.engine.type, self.displacement, self.number_of_wings})' 
 
-    @property
+    @property # возможности робота
     def get_capability(self):                           # самореклама - а я еще могу ...
-        print(f"Возможности: ", self.__repr__)
-    
+        print(f"Характеристики робота: ", self.__repr__)
+        self.can_fly
+        self.can_float
+            
