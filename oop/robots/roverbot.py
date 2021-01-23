3b843a17552045a215a9a1ed5f30c1d9d468b20a
Aleksander <petr0vskjy.aleksander@gmail.com>
2021-01-23 19:11:51 +0300

    Update roverbot.py

--- oop/robots/roverbot.py
+++ oop/robots/roverbot.py
@@ -1,12 +1,20 @@
-
 from .fatherbot import FatherBot
 from .bot import Bot
 from .candrive import CanDrive
+from  .engine     import Engine, base_engine, aviation_engine, marine_engine, hi_power_engine 
 class RoverBot(Bot, CanDrive):
-    def __init__(self, model, manufacture, name, weight, engine_power, fuel_supply, fuel_rate, number_of_wheels):
+    """
+    Робот для движения по поверхности планеты
+    """
+    def __init__(self, model, manufacture, name, weight, fuel_supply, engine, number_of_wheels):
         FatherBot.__init__(self, model, manufacture)
-        Bot.__init__(self, model, manufacture, name, weight, engine_power, fuel_supply, fuel_rate)
+        Bot.__init__(self, model, manufacture, name, weight, fuel_supply, engine=base_engine)
         CanDrive.__init__(self, number_of_wheels)
 
     def __repr__(self):
-        return f'{self.__class__.__name__}("{self.model, self.manufacture, self.name, self.weight, self.engine_power, self.fuel_supply, self.fuel_rate, self.number_of_wheels})' 
+        return f'{self.__class__.__name__}("{self.model, self.manufacture, self.name, self.weight, self.fuel_supply, self.engine.type, self.number_of_wheels})' 
+    
+    @property
+    def get_capability(self):                           # самореклама - а я еще могу ...
+        print(f"Характеристики робота: ", self.__repr__)
+        self.can_drive
