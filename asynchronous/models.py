463b1b59ddca595c87b3474f87f123eb1f3a9a3b
Aleksander <petr0vskjy.aleksander@gmail.com>
2021-02-05 10:53:15 +0300

    Update models.py

--- asynchronous/models.py
+++ asynchronous/models.py
@@ -1,1 +1,23 @@
 
+from tortoise.models import Model
+from tortoise import fields, Tortoise
+
+class Weather(Model):
+    """
+    описывает погоду в городе:
+    name:название_города, temp:температура F, pressure:давление, humidity:влажность 
+    """
+    id   = fields.IntField(pk=True)
+    name = fields.CharField(max_length=255)
+    temp = fields.FloatField() 
+    pressure = fields.FloatField() 
+    humidity = fields.FloatField()
+
+    class Meta:
+        table = "weather" 
+
+    def __str__(self):
+        return f"{self.__class__.__name__} name:{self.name}, id: {self.id}, temp:{self.temp}, pressure:{self.pressure}, humidity: {self.humidity}"
+
+    def __repr__(self):
+        return self.__str__()
