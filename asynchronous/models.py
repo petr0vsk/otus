ce94739052fca624329a0662e22ba046bebffaf5
Aleksander <petr0vskjy.aleksander@gmail.com>
2021-02-14 15:15:34 +0300

    Update models.py

--- asynchronous/models.py
+++ asynchronous/models.py
@@ -1,23 +1,29 @@
-
 from tortoise.models import Model
-from tortoise import fields, Tortoise
+from tortoise import fields, Tortoise, run_async
+import logging
+
 
 class Weather(Model):
     """
-    описывает погоду в городе:
-    name:название_города, temp:температура F, pressure:давление, humidity:влажность 
+    описывает погоду в городе,
+    название_города, температура F, давление, влажность 
     """
-    id   = fields.IntField(pk=True)
-    name = fields.CharField(max_length=255)
-    temp = fields.FloatField() 
+    id       = fields.IntField(pk=True)
+    datetime = fields.DatetimeField(auto_now_add=True)
+    name     = fields.CharField(max_length=255)
+    temp     = fields.FloatField() 
     pressure = fields.FloatField() 
     humidity = fields.FloatField()
 
+    
     class Meta:
         table = "weather" 
 
     def __str__(self):
-        return f"{self.__class__.__name__} name:{self.name}, id: {self.id}, temp:{self.temp}, pressure:{self.pressure}, humidity: {self.humidity}"
+        return f"{self.__class__.__name__} id: {self.id}, datetime:{self.datetime},  name:{self.name},  temp:{self.temp}, pressure:{self.pressure}, humidity: {self.humidity}"
 
     def __repr__(self):
         return self.__str__()
+
+
+
