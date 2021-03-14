970cc4b53a44b8d3fea1fdad1e0b2dd2d5a64a0c
Aleksander <petr0vskjy.aleksander@gmail.com>
2021-03-14 15:19:37 +0300

    Add files via upload

--- task23_flask/landing/__init__.py
+++ task23_flask/landing/__init__.py
@@ -1,1 +1,21 @@
+import os
+from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+
+
+
+app = Flask(__name__)
+csrf = CSRFProtect(app)
+
+
+
+app.config.update(dict(
+    SECRET_KEY="4b332#@@1!74-006b-4889-8#@#-67b#@#b90ddffd",
+    WTF_CSRF_SECRET_KEY='8207bece0139#4980!af20a681e3bf56f3',
+   
+))
+
+
+from .home.views import *
+from .views import *
 
