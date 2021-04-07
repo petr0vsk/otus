f5f6e8542a444f6ef44896dd23173aa2b1e452af
Aleksander <petr0vskjy.aleksander@gmail.com>
2021-04-07 09:48:06 +0300

    Add files via upload

---
+++ task29/task/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField('Навзвание', max_length=128, blank=True)
+    task = models.TextField('Описание', blank=True)
+
+    
+    def __str__(self):
+        return self.title
