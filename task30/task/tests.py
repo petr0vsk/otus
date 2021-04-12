c0d7cc7429bb1696af8c7bc963e06e54803845bb
Aleksander <petr0vskjy.aleksander@gmail.com>
2021-04-12 21:57:16 +0300

    add setUP to tests.py

--- task30/task/tests.py
+++ task30/task/tests.py
@@ -1,10 +1,16 @@
 from django.test import TestCase
 from .models import Task
 
-
 class TestTask(TestCase):
+
+    def setUp(self):
+        self.test_title = 'Главная'
+       
+        
+    def tearDown(self):
+        del self.test_title
 
 
     def test_count_empty(self):
-        task = Task.objects.create(title = "123")
+        task = Task.objects.create(title = self.test_title)
         self.assertEqual(task.task_count(), 1)
